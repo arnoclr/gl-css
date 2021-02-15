ec2ab3b061155946fce085691b5f8157fa39612c
Arno Cellarier <45895096+Aryqs-Ipsum@users.noreply.github.com>
2021-02-15 19:10:55 +0100

    add text fields

--- build/gl.js
+++ build/gl.js
@@ -1,5 +1,15 @@
+// drawer
+
 document.querySelector('.js-drawer-trigger').addEventListener('click', () => {
-    document.querySelector('aside.drawer').classList.toggle('small');
+    if (window.innerWidth < 600) {
+        document.querySelector('aside.drawer.modal').classList.toggle('open')
+    } else {
+        document.querySelector('aside.drawer').classList.toggle('small')
+    }
+})
+
+document.querySelector('.js-drawer-closable').addEventListener('click', () => {
+    document.querySelector('aside.drawer.modal').classList.remove('open')
 })
 
 document.querySelectorAll('.drawer .item').forEach(a => {
@@ -10,6 +20,25 @@ document.querySelectorAll('.drawer .item').forEach(a => {
         a.classList.add('active')
     })
 })
+
+function _setDrawerMode() {
+    document.querySelector('aside.drawer').classList.remove('modal')
+    document.querySelector('aside.drawer').classList.remove('small')
+    if (window.innerWidth < 600) {
+        document.querySelector('aside.drawer').classList.remove('open')
+        document.querySelector('aside.drawer').classList.add('modal')
+    } else if (window.innerWidth < 960) {
+        document.querySelector('aside.drawer').classList.add('open')
+        document.querySelector('aside.drawer').classList.add('small')
+    } else {
+        document.querySelector('aside.drawer').classList.add('open')
+    }
+}
+
+_setDrawerMode()
+window.addEventListener('resize', _setDrawerMode)
+
+// modal
 
 document.querySelectorAll('.js-modal-trigger').forEach(a => {
     a.addEventListener('click', () => {
