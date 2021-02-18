264708f5c85138da713452f9d33bf9630800bdc0
Aryqs-Ipsum <mrfeyurikenpro@gmail.com>
2021-02-18 12:06:38 +0100

    add messages example, menus & update panels

--- build/gl.js
+++ build/gl.js
@@ -1,68 +1,115 @@
-// drawer
+window.addEventListener('DOMContentLoaded', e => {
 
-document.querySelector('.js-drawer-trigger').addEventListener('click', () => {
-    if (window.innerWidth < 600) {
-        document.querySelector('aside.drawer.modal').classList.toggle('open')
-    } else {
-        document.querySelector('aside.drawer').classList.toggle('small')
-    }
-})
+    // drawer
 
-document.querySelector('.js-drawer-closable').addEventListener('click', () => {
-    document.querySelector('aside.drawer.modal').classList.remove('open')
-})
-
-document.querySelectorAll('.drawer .item').forEach(a => {
-    a.addEventListener('click', e => {
-        document.querySelectorAll('.drawer .item').forEach(i => {
-            i.classList.remove('active')
+    var _d_t = document.querySelector('.js-drawer-trigger')
+    if(_d_t) {
+        _d_t.addEventListener('click', () => {
+            if (window.innerWidth < 600) {
+                document.querySelector('aside.drawer.modal').classList.toggle('open')
+            } else {
+                document.querySelector('aside.drawer').classList.toggle('small')
+            }
         })
-        a.classList.add('active')
-    })
-})
+    }
 
-function _setDrawerMode() {
-    if (window.innerWidth < 600) {
-        document.querySelector('aside.drawer').classList.remove('small')
-        document.querySelector('aside.drawer').classList.remove('open')
-        document.querySelector('aside.drawer').classList.add('modal')
-    } else {
-        document.querySelector('aside.drawer').classList.remove('modal')
-        if (window.innerWidth < 960) {
-            document.querySelector('aside.drawer').classList.add('open')
-            document.querySelector('aside.drawer').classList.add('small')
+    var _d_c = document.querySelector('.js-drawer-closable')
+    if(_d_c) {
+        _d_c.addEventListener('click', () => {
+            document.querySelector('aside.drawer.modal').classList.remove('open')
+        })
+    }
+
+    var _d_is = document.querySelectorAll('.drawer .item')
+    if(_d_is) {
+        _d_is.forEach(a => {
+            a.addEventListener('click', e => {
+                document.querySelectorAll('.drawer .item').forEach(i => {
+                    i.classList.remove('active')
+                })
+                a.classList.add('active')
+            })
+        })
+    }
+
+    var _d = document.querySelector('aside.drawer')
+    function _setDrawerMode() {
+        if (window.innerWidth < 600) {
+            _d.classList.remove('small')
+            _d.classList.remove('open')
+            _d.classList.add('modal')
         } else {
-            document.querySelector('aside.drawer').classList.remove('small')
-            document.querySelector('aside.drawer').classList.add('open')
+            _d.classList.remove('modal')
+            if (window.innerWidth < 960) {
+                _d.classList.add('open')
+                _d.classList.add('small')
+            } else {
+                _d.classList.remove('small')
+                _d.classList.add('open')
+            }
         }
     }
-}
 
-_setDrawerMode()
-window.addEventListener('resize', _setDrawerMode)
+    if(_d) {
+        _setDrawerMode()
+        window.addEventListener('resize', _setDrawerMode)
+    }
 
-// modal
+    // modal
 
-document.querySelectorAll('.js-modal-trigger').forEach(a => {
-    a.addEventListener('click', () => {
-        document.querySelector(a.getAttribute("href")).classList.add('open')
+    document.querySelectorAll('.js-modal-trigger').forEach(a => {
+        a.addEventListener('click', () => {
+            document.querySelector(a.getAttribute("href")).classList.add('open')
+        })
     })
-})
 
-document.querySelectorAll('aside.modal .content').forEach(e => {
-    e.addEventListener('click', e => {
-        e.stopPropagation()
+    document.querySelectorAll('aside.modal .content').forEach(e => {
+        e.addEventListener('click', e => {
+            e.stopPropagation()
+        })
     })
-})
 
-document.querySelectorAll('.js-modal-closable, .js-modal-close').forEach(e => {
-    e.addEventListener('click', () => {
-        e.classList.remove('open')
+    document.querySelectorAll('.js-modal-closable, .js-modal-close').forEach(e => {
+        e.addEventListener('click', () => {
+            e.classList.remove('open')
+        })
     })
-})
 
-// loader
+    // loader
 
-document.querySelectorAll('.js-circular-loader').forEach(l => {
-    l.innerHTML += '<svg class="loader" width="42" height="42" xmlns="http://www.w3.org/2000/svg"><g><ellipse ry="16" rx="16" cy="21" cx="21" stroke-width="4" fill="none"/></g></svg>'
+    document.querySelectorAll('.js-circular-loader').forEach(l => {
+        l.innerHTML += '<svg class="loader" width="42" height="42" xmlns="http://www.w3.org/2000/svg"><g><ellipse ry="16" rx="16" cy="21" cx="21" stroke-width="4" fill="none"/></g></svg>'
+    })
+
+    // panel
+
+    var _pan = document.querySelector('aside.panel')
+    if (_pan.dataset.width) {
+        _pan.style.maxWidth = _pan.dataset.width
+        document.querySelector('main').style.marginLeft = _pan.dataset.width
+    }
+
+    // menus
+    var _men = document.querySelectorAll('.js-menu'),
+        _men_tr = document.querySelectorAll('.js-menu-trigger')
+    _men_tr.forEach(t => {
+        t.addEventListener('click', e => {
+            m = document.getElementById(t.dataset.menu)
+            m.classList.add('open')
+            window.addEventListener('click', e => {
+                if (!m.contains(e.target)) {
+                    a = false
+                    e.path.some(p => {
+                        if (p.classList != undefined && p.classList.contains('js-menu-trigger')) {
+                            return a = true
+                        }
+                    })
+                    if (!a) {
+                        m.classList.remove('open')
+                    }
+                }
+            })
+        })
+    })
+
 })
